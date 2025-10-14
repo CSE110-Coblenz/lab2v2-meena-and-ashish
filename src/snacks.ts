71238f4008604d5f6b409bc266c33b002894c2a2
Meenakshi Manikandan <2mmanikan@gmail.com>
2025-10-14 16:46:16 -0700

    added all final snacks

--- src/snacks.ts
+++ src/snacks.ts
@@ -1,28 +1,23 @@
-<<<<<<< HEAD
 import { announce } from "./animation";
 
 const snacksB: string[] = ["Chips", "Cookies", "Granola Bar", "Trail Mix", "Popcorn"];
 
 const snacksC: string[] = ["Nutella", "Milky Way", "Snickers"];
 
-=======
 
-const snacksB: string[] = ["Chips", "Cookies", "Granola Bar", "Trail Mix", "Popcorn"];
 
->>>>>>> 4e1152232b9e5b10c478b18f87b3cb05e7e247be
 export function printSnacks(): void {
   console.log("Available Snacks:");
   for (const snack of snacksB) {
     console.log(`- ${snack}`);
   }
-<<<<<<< HEAD
 
+
+  //print snacksC
   for(const snack2 of snacksC){
 
     console.log(`- ${snack2}`);
   }
-=======
->>>>>>> 4e1152232b9e5b10c478b18f87b3cb05e7e247be
 }
 printSnacks();
 
@@ -32,9 +27,6 @@ export function print_snacks(){
    console.log(snacks);
 }
 
-<<<<<<< HEAD
 announce("snacks", "bold"); 
-=======
->>>>>>> 4e1152232b9e5b10c478b18f87b3cb05e7e247be
 print_snacks();
 
