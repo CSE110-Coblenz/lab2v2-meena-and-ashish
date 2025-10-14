4dc9e0e6fb43a1b1cb22f2a8ca1625ce7af2f671
Ashish Bamba <ashishvbamba@gmail.com>
2025-10-14 16:22:26 -0700

    Add Mosh Bars

--- src/snacks.ts
+++ src/snacks.ts
@@ -1,6 +1,6 @@
 import { announce } from "./animation";
 
-const snacksB: string[] = ["Chips", "Cookie", "Granola Bar", "Trail Mix", "Popcorn"];
+const snacksB: string[] = ["Chips", "Cookie", "Granola Bar", "Trail Mix", "Popcorn", "Mosh Bars"];
 
 export function printSnacks(): void {
   console.log("Available Snacks:");
